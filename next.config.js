f96f43fa56bf4152741f868387ecca80a5d775ee
Jacob Eckert <jacobeckert@Computron-Strikes-Again.local>
2023-02-20 17:57:28 -0600

    Setup .html redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,51 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/about.html',
+        destination: '/about',
+        permanent: true,
+      },
+      {
+        source: '/contact.html',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/family-law.html',
+        destination: '/family-law',
+        permanent: true,
+      },
+      {
+        source: '/legal-disclaimer.html',
+        destination: '/legal-disclaimer',
+        permanent: true,
+      },
+      {
+        source: '/media.html',
+        destination: '/media',
+        permanent: true,
+      },
+      {
+        source: '/personal-injury.html',
+        destination: '/personal-injury',
+        permanent: true,
+      },
+      {
+        source: '/probate.html',
+        destination: '/probate',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
